574782c08be412980a8827f3da1c08929acbd9fc
Žiga Miklošič <ziga.miklosic@seltron.si>
2022-09-12 13:43:33 +0200

    - implementation of printf

--- src/cli.c
+++ src/cli.c
@@ -19,7 +19,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
 #include "cli.h"
+#include "../../cli_cfg.h"
+#include "../../cli_if.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // Definitions
@@ -29,14 +37,64 @@
 // Variables
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * 		Initialization guard
+ */
+static bool gb_is_init = false;
+
+/**
+ * 		Transmit buffer for printf
+ */
+static uint8_t gu8_tx_buffer[CLI_CFG_PRINTF_BUF_SIZE] = {0};
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Function prototypes
 ////////////////////////////////////////////////////////////////////////////////
+static cli_status_t cli_send_str(const uint8_t * const p_str);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 ////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/*!
+* @brief        Send string
+*
+* @param[in]    p_str	- Pointer to string
+* @return       status	- Status of operation
+*/
+////////////////////////////////////////////////////////////////////////////////
+static cli_status_t cli_send_str(const uint8_t * const p_str)
+{
+	cli_status_t status = eCLI_OK;
+
+	#if ( 1 == CLI_CFG_MUTEX_EN )
+
+		// Mutex obtain
+		if ( eCLI_OK == cli_if_aquire_mutex())
+		{
+			// Write to cli port
+			status |= cli_if_transmit( p_str );
+
+			// Release mutex if taken
+			status |= cli_if_release_mutex();
+		}
+		else
+		{
+			status = eCLI_ERROR;
+		}
+
+	#else
+
+		// Write to cli port
+		cli_if_transmit( p_str );
+
+	#endif
+
+	return status;
+}
 
 
 
@@ -59,6 +117,22 @@ cli_status_t cli_init(void)
 {
 	cli_status_t status = eCLI_OK;
 
+	if ( false == gb_is_init )
+	{
+		// Initialize interface
+		status = cli_if_init();
+
+		if ( eCLI_OK == status )
+		{
+			gb_is_init = true;
+		}
+	}
+	else
+	{
+		status = eCLI_ERROR_INIT;
+	}
+
+	CLI_ASSERT( eCLI_OK == status );
 
 	return status;
 }
@@ -68,6 +142,21 @@ cli_status_t cli_deinit(void)
 {
 	cli_status_t status = eCLI_OK;
 
+	if ( true == gb_is_init )
+	{
+		status = cli_if_deinit();
+
+		if ( eCLI_OK == status )
+		{
+			gb_is_init = false;
+		}
+	}
+	else
+	{
+		status = eCLI_ERROR_INIT;
+	}
+
+	CLI_ASSERT( eCLI_OK == status );
 
 	return status;
 }
@@ -77,7 +166,16 @@ cli_status_t cli_is_init(bool * const p_is_init)
 {
 	cli_status_t status = eCLI_OK;
 
+	CLI_ASSERT( NULL != p_is_init  );
 
+	if ( NULL != p_is_init )
+	{
+		*p_is_init = gb_is_init;
+	}
+	else
+	{
+		status = eCLI_ERROR;
+	}
 
 	return status;
 }
@@ -94,8 +192,19 @@ cli_status_t cli_hndl(void)
 
 cli_status_t cli_printf(const uint8_t * p_format, ...)
 {
-	cli_status_t status = eCLI_OK;
+	cli_status_t 	status = eCLI_OK;
+	va_list 		args;
 
+	// Taking args from stack
+	va_start(args, p_format);
+	vsprintf((char*) gu8_tx_buffer, p_format, args);
+	va_end(args);
+
+	// Add line termination and print message
+	strcat( (char*)gu8_tx_buffer, (char*) CLI_CFG_TERMINATION_STRING );
+
+	// Send string
+	status = cli_send_str((const uint8_t*) &gu8_tx_buffer);
 
 	return status;
 }
