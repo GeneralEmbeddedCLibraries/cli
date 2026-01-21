c3beedbeda14fe4c6e82a29cdbe6445094c033b6
Žiga Miklošič <ziga.miklosic@gmail.com>
2026-01-21 13:20:44 +0100

    - improvement of error status printing

--- src/cli_par.c
+++ src/cli_par.c
@@ -283,49 +283,42 @@ static void cli_par_set(const cli_cmd_t * p_cmd, const char * p_attr)
                         case ePAR_TYPE_U8:
                             (void) sscanf( p_attr, "%hu,%hhu", &par_id, &par_data.u8 );
                             status = par_set( par_num, &par_data.u8 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%u", par_data.u8);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%u", par_data.u8);
                         break;
 
                         case ePAR_TYPE_I8:
                             sscanf( p_attr, "%hu,%hhi", &par_id, &par_data.i8 );
                             status = par_set( par_num, &par_data.i8 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%i", par_data.i8);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%i", par_data.i8);
                         break;
 
                         case ePAR_TYPE_U16:
                             sscanf( p_attr, "%hu,%hu", &par_id, &par_data.u16 );
                             status = par_set( par_num, &par_data.u16 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%u", par_data.u16);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%u", par_data.u16);
                         break;
 
                         case ePAR_TYPE_I16:
                             sscanf( p_attr, "%hu,%hi", &par_id, &par_data.i16 );
                             status = par_set( par_num, &par_data.i16 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%i", par_data.i16);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%i", par_data.i16);
                         break;
 
                         case ePAR_TYPE_U32:
                             sscanf( p_attr, "%hu,%u", &par_id, (unsigned int*)&par_data.u32 );
                             status = par_set( par_num, &par_data.u32 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%u", par_data.u32);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%u", par_data.u32);
                         break;
 
                         case ePAR_TYPE_I32:
                             sscanf( p_attr, "%hu,%i", &par_id, (int*)&par_data.i32 );
                             status = par_set( par_num, &par_data.i32 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%i", par_data.i32);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%i", par_data.i32);
                         break;
 
                         case ePAR_TYPE_F32:
                             status = par_set( par_num, &par_data.f32 );
-                            if (ePAR_OK == status)  cli_printf( "OK,PAR_SET=%g", par_data.f32);
-                            else                    cli_printf( "ERR, error code=0x%X", (par_status_t)status);
+                            if (ePAR_OK == status) cli_printf( "OK,PAR_SET=%g", par_data.f32);
                         break;
 
                         case ePAR_TYPE_NUM_OF:
@@ -334,6 +327,11 @@ static void cli_par_set(const cli_cmd_t * p_cmd, const char * p_attr)
                             CLI_ASSERT( 0 );
                         break;
                     }
+
+                    if      ( ePAR_ERROR_VALUE == status ) cli_printf( "ERR, Trying to set invalid parameter value!" );
+                    else if ( ePAR_ERROR_MUTEX == status ) cli_printf( "ERR, Failed to obtain mutex!" );
+                    else if ( ePAR_WAR_LIMITED == status ) cli_printf( "WAR, Parameter value limited to its min/max range!" );
+                    else                                   cli_printf( "%s, Parameter setting code: 0x%X", (status & ePAR_STATUS_ERROR_MASK) ? "ERR" : "WAR", (par_status_t)status);
                 }
                 else
                 {
